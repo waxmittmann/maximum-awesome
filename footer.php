19181630cd786b43861571b719b478e0a4f4d7c6
Max Wittmann <damxam@gmail.com>
2018-04-15 16:30:39 +1000

    ..

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
-<div id= "ttr_footer">
-<h1>FOOTER</h1>
-</div>
-</div>
+</div> <!-- /.container -->
+
+<footer class="blog-footer">
+  <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+  <p>
+    <a href="#">Back to top</a>
+  </p>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
 </body>
 </html>
